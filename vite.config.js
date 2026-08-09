f56dc613fec378919404d6f914804cd2828ec230
maki0315-1617 <ronron201907@gmail.com>
2026-08-09 13:45:28 +0900

    ５

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        // ★ここを追加：CloudflareのプログラムもViteに直接ビルドさせます
-        _worker: './public/_worker.js' 
-      },
-      output: {
-        // ★プログラム名が変わらないように出力ファイル名を固定します
-        entryFileNames: (chunkInfo) => {
-          return chunkInfo.name === '_worker' ? '[name].js' : 'assets/[name]-[hash].js';
-        }
-      }
-    }
-  }
 })
